65e6a81bd400940f56c76d232c0ac1d5d149ba8c
3v <admin@3v.fi>
2017-01-03 18:50:29 +0000

    fix default sorting in /top, add analytics

--- public/top.js
+++ public/top.js
@@ -3,19 +3,23 @@ $(document).ready(function () {
 
     $.getJSON('api/top', function (json) {
         for (var i = 0; i < tops.length; i += 1) {
+            var data = json.top[tops[i]].map(function (val) {
+                val.name = '<a href="u/' + val.name + '">' + val.name + '</a>';
+                return val;
+            });
             $('#dt_' + tops[i]).DataTable({
                 order: [[i + 1, 'desc']],
                 columns: [
                     { title: 'Moderator', data: 'name' },
                     { title: 'Swords', data: 'modcount' },
-                    { title: 'Total Followers', data: 'followers' },
                     { title: 'Total Views', data: 'views' },
+                    { title: 'Total Followers', data: 'followers' }
                 ],
                 deferRender: true,
                 pageLength: 10,
                 language: {
                     search: '',
-                    searchPlaceholder: ''
+                    searchPlaceholder: 'search'
                 },
                 data: json.top[tops[i]]
             });
